07480eb247a5120980f73c6b23ef81095d8e4870
angel <vellebue@gmail.com>
2025-05-15 21:28:24 +0200

    fix: Updated update.sh file.
    "update.sh" has been fixed to repair an error.

--- environments/devel/minikube/update.sh
+++ environments/devel/minikube/update.sh
@@ -2,4 +2,4 @@
 helm upgrade --namespace devel --values grafana-loki-values.yaml loki grafana/loki-stack
 #UPDATE RABBIT MQ
 #Update Rabbit Mq kubernetes operator
-helm install --namespace devel --values rabbitmq-values.yaml rabbit-mq bitnami/rabbitmq-cluster-operator
+helm upgrade --namespace devel --values rabbitmq-values.yaml rabbit-mq bitnami/rabbitmq-cluster-operator
